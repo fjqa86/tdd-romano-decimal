90a92bcdbb9055a5ba367c8e7431d9efac13cfed
fjqa86 <fjqa86@gmail.com>
2014-06-11 18:29:19 -0500

    primera subida

---
+++ src/co/edu/udea/appempresariales/romano/RomanoDecimal.java
@@ -0,0 +1,5 @@
+package co.edu.udea.appempresariales.romano;
+
+public class RomanoDecimal {
+
+}
---
+++ test/co/edu/udea/appempresariales/romano/RomanoDecimalTest.java
@@ -0,0 +1,19 @@
+package co.edu.udea.appempresariales.romano;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class RomanoDecimalTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
